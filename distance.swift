f2025bfbf2a5df80a86ae8fc9df7a2aac3b00e0d
Nitapol <nitapol@hotmail.com>
2019-03-01 02:30:23 -0800

    Swift 46.7 sec

--- distance.swift
+++ distance.swift
@@ -1,18 +1,7 @@
 //  Copyright (c) 2019 Alexander Lopatin. All rights reserved.
 import Foundation
 
-func penalty(_ c: Character) -> Int {
-    var ascii = 255
-    if c.unicodeScalars.first?.isASCII == true {
-        if let v = c.unicodeScalars.first?.value {
-            let i = Int(Int32(bitPattern: v))
-            if i > 0 && i < 255 {
-                ascii = i
-            }
-        }
-    }
-    return ascii
-}
+func fee(_ c:Character)->Int {return Int(c.unicodeScalars.first?.value ?? 255)}
 
 // Algorithm #1: Brute force.
 func distance1(_ a: String, _ b: String) -> Int {
@@ -21,35 +10,26 @@ func distance1(_ a: String, _ b: String) -> Int {
     }
     var d = Int.max
     for i in a.indices {
-        d = min(d, penalty(a[i]) + distance1(
-            String(a[..<i] + a[a.index(after: i)...]), b))
+        d=min(d,fee(a[i])+distance1(String(a[..<i]+a[a.index(after:i)...]),b))
     }
     for i in b.indices {
-        d = min(d, penalty(b[i]) + distance1(
-            a, String(b[..<i] + b[b.index(after: i)...])))
+        d=min(d,fee(b[i])+distance1(a,String(b[..<i]+b[b.index(after:i)...])))
     }
     return d
 }
 
-assert(distance1("at", "cat") == penalty("c"))
-assert(distance1("bat", "cat") == penalty("b") + penalty("c"))
-assert(distance1("!~!", "~!!") == 2 * penalty("!")) // ! vs ~ (33 vs 126)
-assert(distance1("!!~", "!~!") == 2 * penalty("!"))
-
-var n = 0
-var N = 0
-var total_score = 0
-let date = Date()
+assert(distance1("at", "cat") == fee("c"))
+assert(distance1("bat", "cat") == fee("b") + fee("c"))
+assert(distance1("!~!", "~!!") == 2 * fee("!")) // ! vs ~ (33 vs 126)
+assert(distance1("!!~", "!~!") == 2 * fee("!"))
+var n = 0; var N = 0; var d = 0; let date = Date()
 while n <= 99999 {
-    var s = String(n)
-    while s.count < 5 {
-        s = "0" + s
-    }
+    var s = String(format: "%05d", n)
     func test(_ a: String, _ b: String) {
-        let score1 = distance1(a, b)
-        let score2 = distance1(b, a)
-        total_score += score1
-        assert(score1 == score2)
+        let d1 = distance1(a, b)
+        let d2 = distance1(b, a)
+        d += d1
+        assert(d1 == d2)
         N += 2
     }
     test("", s)
@@ -59,4 +39,4 @@ while n <= 99999 {
     n += 1
 }
 let t = Date().timeIntervalSince(date as Date)
-print("\(t),\(total_score),\(N),\(t.magnitude / Double(N))")
+print("\(t),\(d),\(N),\(t.magnitude/Double(N))")
