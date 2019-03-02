a78e2988083fb4e3fe3b870bdf18d99ddc7769b6
Nitapol <nitapol@hotmail.com>
2019-03-02 05:03:34 -0800

    C 4.4 sec!

--- distance1.c
+++ distance1.c
@@ -6,11 +6,10 @@
 #define MAXSIZE 6
 #define min(a,b) ({__typeof__(a) _a=(a); __typeof__(b) _b=(b); _a<_b ? _a:_b; })
 
-inline int penalty(char c) { return c; }
+inline int fee(char c) { return c; }
 
-char* skip_char(char *dst, const char *src, int skip) {
-    int i = 0, j = 0;
-    char c;
+inline char* skip_char(char *dst, const char *src, int skip) {
+    int i = 0, j = 0; char c;
     while((c = src[i]) != 0)
         if (i++ != skip)
             dst[j++] = c;
@@ -18,18 +17,16 @@ char* skip_char(char *dst, const char *src, int skip) {
     return dst;
 }
 
-int distance1(const char *s1, const char *s2) {
-    char s[MAXSIZE+MAXSIZE];
-    size_t l1 = strlen(s1);
-    size_t l2 = strlen(s2);
-    if (l1 == l2 && strcmp(s1, s2) == 0)
+int distance1(const char *a, const char *b) {
+    size_t l1 = strlen(a), l2 = strlen(b);
+    if (l1 == l2 && strcmp(a, b) == 0)
         return 0;
-    int score = __INT_MAX__, i;
-    for (i = 0; i < l1; i++)
-        score = min(score, penalty(s1[i]) + distance1(skip_char(s,s1,i),s2));
-    for (i = 0; i < l2; i++)
-        score = min(score, penalty(s2[i]) + distance1(s1,skip_char(s,s2,i)));
-    return score;
+    int d = __INT_MAX__; char s[MAXSIZE];
+    for (int i = 0; i < l1; i++)
+        d = min(d, fee(a[i]) + distance1(skip_char(s,a,i), b));
+    for (int i = 0; i < l2; i++)
+        d = min(d, fee(b[i]) + distance1(a, skip_char(s,b,i)));
+    return d;
 }
 
 void assert(int e) { if (e != 1) { printf("*** ERROR %d ***\n", e); exit(1); }}
@@ -37,33 +34,31 @@ void assert(int e) { if (e != 1) { printf("*** ERROR %d ***\n", e); exit(1); }}
 #define LOOP 99999
 #define LEN 5
 #define FORMAT "%05d"
-#define SIZE LEN+1
+#define SIZE (LEN+1)
 
 int main() {
     assert(MAXSIZE >= SIZE);
-    assert(distance1("at", "cat") == penalty('c'));
-    assert(distance1("bat", "cat") == penalty('b') + penalty('c'));
-    assert(distance1("!~!", "~!!") == 2 * penalty('!'));
-    assert(distance1("!!~", "!~!") == 2 * penalty('!'));
+    assert(distance1("bat", "cat") == fee('b') + fee('c'));
+    assert(distance1("!~!", "~!!") == 2 * fee('!'));
     clock_t start = clock();
-    int n = 0, N = 0, total_score = 0, score1, score2, i;
-    char s[SIZE], s1[SIZE], *s2, c;
+    int n = 0, d = 0;
+    char s[SIZE], a[SIZE];
     do {
         sprintf(s, FORMAT, n);
-        for (i = 0; i < SIZE; i++) {
-            c = s[i];
+        for (int i = 0; i < SIZE; i++) {
+            char c = s[i];
             s[i] = 0;
-            strcpy(s1, s);
+            strcpy(a, s);
             s[i] = c;
-            s2 = s + i;
-            score1 = distance1(s1, s2);
-            score2 = distance1(s2, s1);
-            assert(score1 == score2);
-            total_score += score1;
-            N += 2;
+            char *b = s + i;
+            int da = distance1(a, b);
+            int db = distance1(b, a);
+            assert(da == db);
+            d += da;
         }
     } while (n++ < LOOP);
+    n *= 2*SIZE;
     double t = (clock() - start) / (double) CLOCKS_PER_SEC;
-    printf("%f,%d,%d,%f\n", t, total_score, N, t/N);
+    printf("%f,%d,%d,%f\n", t, d, n, t/n);
     return 0;
 }
