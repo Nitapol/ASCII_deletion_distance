b7a2500703106716deb0e4df5fd2b9327d72051e
Nitapol <nitapol@hotmail.com>
2019-03-01 19:14:59 -0800

    Python 3.5 355.2 sec; Python 3.7 934.2 (The Final Countdown for ver1)

--- distance1.py
+++ distance1.py
@@ -1,50 +1,41 @@
 # Copyright (c) 2019 Alexander Lopatin. All rights reserved.
 import time
-import platform
 import sys
+# a, b, e, f, s - strings
+# c - character;
+# i, j, n, d - integers for indices , numbers, distances
 
 
-def penalty(char): return ord(char)
+def fee(c): return ord(c)
 
 
-def distance1(str1, str2):  # Algorithm #1 (Reference): Brute force.
-    if str1 == str2:
+def distance1(a, b):  # Algorithm #1 (Reference): Brute force.
+    if a == b:
         return 0
-    score = sys.maxsize  # Enough for the worst sum of 8421504 bytes in 32bit.
-    for j, c in enumerate(str1):
-        score = min(score, penalty(c) + distance1(str1[:j]+str1[j+1:], str2))
-    for j, c in enumerate(str2):
-        score = min(score, penalty(c) + distance1(str1, str2[:j]+str2[j+1:]))
-    return score
+    d = sys.maxsize  # Enough for the worst sum of 8421504 bytes in 32bit.
+    for i, c in enumerate(a):
+        d = min(d, fee(c) + distance1(a[:i]+a[i+1:], b))
+    for i, c in enumerate(b):
+        d = min(d, fee(c) + distance1(a, b[:i]+b[i+1:]))
+    return d
 
 
 if __name__ == '__main__':
-    print(platform.node())
-    (mac_ver, _, _) = platform.mac_ver()
-    if mac_ver is not None and mac_ver != "":
-        print("macOS version", mac_ver)
-    print(platform.platform())
-    print("Python", platform.python_build(), platform.python_compiler())
-    print("Executing in", "64bit" if sys.maxsize > 2 ** 32 else "32bit")
-
-    assert distance1("at", "cat") == penalty('c')
-    assert distance1("bat", "cat") == penalty('b') + penalty('c')
-    assert distance1("!~!", "~!!") == 2 * penalty('!')  # ! vs ~ (33 vs 126)
-    assert distance1("!!~", "!~!") == 2 * penalty('!')
+    assert distance1("cool", "cold") == fee('o') + fee('d')
+    assert distance1("!~!", "~!!") == 2 * fee('!')  # ! vs ~ (33 vs 126)
     t = time.process_time()
     n = 0
-    N = 0
-    total_score = 0
-    while n <= 99999:
+    dt = 0
+    while n < 100000:
         s = "{0:05d}".format(n)
-        for i in range(len(s) + 1):
-            s1 = s[:i]
-            s2 = s[i:]
-            score1 = distance1(s2, s1)
-            score2 = distance1(s1, s2)
-            N += 2
-            total_score += score1
-            assert(score1 == score2)
+        for j in range(6):
+            e = s[:j]
+            f = s[j:]
+            de = distance1(e, f)
+            df = distance1(f, e)
+            dt += de
+            assert(de == df)
         n += 1
+    n = n * 12
     t = time.process_time() - t
-    print(t, total_score, N, t/N, sep=',')
+    print(t, dt, n, t/n, sep=',')
