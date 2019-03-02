37c28f09b9be8a64ca6eb05bd0c7b6ae82bc4f83
Nitapol <nitapol@hotmail.com>
2019-03-02 04:24:11 -0800

    C++ 4.7 sec!

--- distance1.cpp
+++ distance1.cpp
@@ -2,10 +2,10 @@
 #include <iostream>
 #include <sstream>
 #include <iomanip>
-using namespace std;
 #include <string>
+using namespace std;
 
-int penalty(const char& c) { return c; }
+int fee(const char& c) { return c; }
 
 string& skip_char(string &dst, const string &src, int skip) {
     dst.clear();
@@ -23,9 +23,9 @@ int distance1(const string &a, const string &b) {
     int d = __INT_MAX__;
     string s;
     for (int i = 0; i < la; i++)
-        d = min(d, penalty(a[i]) + distance1(skip_char(s, a, i), b));
+        d = min(d, fee(a[i]) + distance1(skip_char(s, a, i), b));
     for (int i = 0; i < lb; i++)
-        d = min(d, penalty(b[i]) + distance1(a, skip_char(s, b, i)));
+        d = min(d, fee(b[i]) + distance1(a, skip_char(s, b, i)));
     return d;
 }
 
@@ -33,27 +33,25 @@ int distance1(const string &a, const string &b) {
 #define LEN 5
 
 int main() {
-    assert(distance1("at", "cat") == penalty('c'));
-    assert(distance1("bat", "cat") == penalty('b') + penalty('c'));
-    assert(distance1("!~!", "~!!") == 2 * penalty('!'));
-    assert(distance1("!!~", "!~!") == 2 * penalty('!'));
+    assert(distance1("bat", "cat") == fee('b') + fee('c'));
+    assert(distance1("!~!", "~!!") == 2 * fee('!'));
     clock_t start = clock();
-    int n = 0, N = 0, d = 0;
+    int n = 0, d = 0;
     do {
         stringstream ss;
         ss << std::setw(LEN) << std::setfill('0') << n;
         string s = ss.str();
-        for (int i = 0; i < LEN + 1; i++) {
+        for (int i = 0; i < LEN+1; i++) {
             string a = s.substr(0, i);
             string b = s.substr(i);
-            int d1 = distance1(a, b);
-            int d2 = distance1(b, a);
-            assert(d1 == d2);
-            d += d1;
-            N += 2;
-         }
+            int da = distance1(a, b);
+            int db = distance1(b, a);
+            assert(da == db);
+            d += da;
+        }
     } while (n++ < LOOP);
+    n *= 2*(LEN+1);
     double t = (clock() - start) / (double) CLOCKS_PER_SEC;
-    cout << t << "," << d << "," << N << "," << t/N << endl;
+    cout << t << "," << d << "," << n << "," << t/n << endl;
     return 0;
 }
