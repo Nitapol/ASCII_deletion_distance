73a8d60ccdea1fd279b4f0f1147d66c8a671937d
Nitapol <nitapol@hotmail.com>
2019-03-02 17:42:48 -0800

    C# 29.1 sec

--- distance1.cs
+++ distance1.cs
@@ -1,66 +1,58 @@
 // Copyright (c) 2019 Alexander Lopatin. All rights reserved.
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 
-namespace deletionDistanceCSharp
-{
-    class MainClass
-    {
-        static int Penalty(char c) { return c; }
+namespace deletionDistanceCSharp {
+    class MainClass {
+        static int Fee(char c) { return c; }
 
-        static string Remove(string s, int i)
-        {
+        static string Remove(string s, int i) {
             string r = i > 0 ? s.Substring(0, i) : "";
             if (i < s.Length)
                 r += s.Substring(i + 1);
             return r;
         }
 
-        static int Distance1(string a, string b)
-        {
+        static int Distance1(string a, string b) {
             if (a == b)
                 return 0;
-            int score = int.MaxValue;
+            int d = int.MaxValue;
             for (int i = 0; i < a.Length; i++)
-                score = Math.Min(score, Penalty(a[i])+Distance1(Remove(a,i),b));
+                d = Math.Min(d, Fee(a[i])+Distance1(Remove(a,i),b));
             for (int i = 0; i < b.Length; i++)
-                score = Math.Min(score, Penalty(b[i])+Distance1(a,Remove(b,i)));
-            return score;
+                d = Math.Min(d, Fee(b[i])+Distance1(a,Remove(b,i)));
+            return d;
         }
 
-        static void assert(bool ok)
-        {
+        static void assert(bool ok) {
             if (!ok)
                 Console.WriteLine("*************** ERROR ***************");
         }
 
-        public static void Main(string[] args)
-        {
-            assert(Distance1("at", "cat") == Penalty('c'));
-            assert(Distance1("bat", "cat") == Penalty('b') + Penalty('c'));
-            assert(Distance1("!~!", "~!!") == 2 * Penalty('!'));
-            assert(Distance1("!!~", "!~!") == 2 * Penalty('!'));
+        public static void Main(string[] args) {
+            assert(Distance1("bat", "cat") == Fee('b') + Fee('c'));
+            assert(Distance1("!~!", "~!!") == 2 * Fee('!'));
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
-            int n = 0, N = 0, total_score = 0;
-            while (n <= 99999) {
-                string s = n.ToString("D5");
-                for (int i = 0; i <= s.Length; i++) {
+            const int len = 5;
+            int n = 0, d = 0;
+            do
+            {
+                string s = n.ToString("D" + len.ToString());
+                for (int i = 0; i <= len; i++)
+                {
                     string a = s.Substring(0, i);
                     string b = s.Substring(i);
-                    int score1 = Distance1(a, b);
-                    int score2 = Distance1(a, b);
-                    N += 2;
-                    total_score += score1;
-                    assert(score1 == score2);
+                    int da = Distance1(a, b);
+                    int db = Distance1(a, b);
+                    d += da;
+                    assert(da == db);
                 }
-                n += 1;
-            }
+            } while (n++ < 99999);
+            n *= 2 * (len + 1);
             stopWatch.Stop();
-            //            TimeSpan ts = stopWatch.Elapsed;
             Double t = stopWatch.Elapsed.TotalSeconds;
-            Console.Write(t+"," +total_score+"," + N+"," + t/N);
+            Console.Write(t+"," +d+"," + n+"," + t/n);
             Console.WriteLine();
         }
     }
