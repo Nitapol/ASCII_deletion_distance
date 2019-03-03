4d0877dba9eec7832d8e886821727e938dbdc379
Nitapol <nitapol@hotmail.com>
2019-03-02 20:33:44 -0800

    Swift 7.46 sec! [UInt8] instead String for four times faster!!

--- distance1.swift
+++ distance1.swift
@@ -1,30 +1,60 @@
 //  Copyright (c) 2019 Alexander Lopatin. All rights reserved.
 import Foundation
 
-func fee(_ c:Character)->Int {return Int(c.unicodeScalars.first?.value ?? 255)}
+func fee(_ c: Character) -> Int {
+    var i = 255
+    if let v = c.unicodeScalars.first?.value {
+        i = Int(v)
+        if i < 0 || i > 255 {
+            i = 255
+        }
+    }
+    return i
+}
 
 // Algorithm #1: Brute force.
-func distance1(_ a: String, _ b: String) -> Int {
+func distance1a(_ a: inout [UInt8], _ b: inout [UInt8]) -> Int {
     if a == b {
         return 0
     }
     var d = Int.max
     for i in a.indices {
-        d=min(d,fee(a[i])+distance1(String(a[..<i]+a[a.index(after:i)...]),b))
+        let n = a.remove(at: i)
+        d = min(d, Int(n) + distance1a(&a , &b))
+        a.insert(n, at: i)
     }
     for i in b.indices {
-        d=min(d,fee(b[i])+distance1(a,String(b[..<i]+b[b.index(after:i)...])))
+        let n = b.remove(at: i)
+        d = min(d, Int(n) + distance1a(&a , &b))
+        b.insert(n, at: i)
     }
     return d
 }
 
+extension String {
+    func byteArray() -> [UInt8] {
+        var a = [UInt8]()
+        for c in self.unicodeScalars {
+            a.append(c.value < UInt8.max ? UInt8(c.value) :UInt8.max)
+        }
+        return a
+    }
+}
+
+func distance1(_ a: String, _ b: String) -> Int {
+    var aA = a.byteArray()
+    var bA = b.byteArray()
+    return distance1a(&aA, &bA)
+}
+
+assert(distance1("at", "cat") == fee("c"))
 assert(distance1("cool", "cold") == fee("o") + fee("d"))
 assert(distance1("!~!", "~!!") == 2 * fee("!")) // ! vs ~ (33 vs 126)
 
 let date = Date()
 var d = 0
 var n = 0
-while n <= 100000 {
+while n < 100000 {
 
     func test(_ a: String, _ b: String) {
         let da = distance1(a, b)
@@ -33,12 +63,13 @@ while n <= 100000 {
         assert(da == db)
     }
 
-    var s = String(format: "%05d", n)
+    let s = String(format: "%05d", n)
     test("", s)
     for i in s.indices {
         test(String(s[...i]), String(s[s.index(after: i)...]))
     }
     n += 1
 }
+n *= 12
 let t = Date().timeIntervalSince(date as Date)
-print("\(t),\(d),\(N),\(t.magnitude/Double(N))")
+print("\(t),\(d),\(n),\(t.magnitude/Double(n))")
